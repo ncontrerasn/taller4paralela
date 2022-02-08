9dc0751b2d61daca261c843f9fd58cc8d4320615
ncontrerasn <36159522+ncontrerasn@users.noreply.github.com>
2022-02-07 22:09:36 -0500

    Update script_ejecutar_todo.sh

--- script_ejecutar_todo.sh
+++ script_ejecutar_todo.sh
@@ -2,21 +2,41 @@
 
 sudo mpic++ taller4.cpp -o t4 `pkg-config --cflags --libs opencv`
 
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 1 --hostfile mpi_hosts ./t4 720p.jpg 720psobel.jpg
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 2 --hostfile mpi_hosts ./t4 720p.jpg 720psobel.jpg
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 3 --hostfile mpi_hosts ./t4 720p.jpg 720psobel.jpg
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 4 --hostfile mpi_hosts ./t4 720p.jpg 720psobel.jpg
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 5 --hostfile mpi_hosts ./t4 720p.jpg 720psobel.jpg
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 6 --hostfile mpi_hosts ./t4 720p.jpg 720psobel.jpg
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 8 --hostfile mpi_hosts ./t4 720p.jpg 720psobel.jpg
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 9 --hostfile mpi_hosts ./t4 720p.jpg 720psobel.jpg
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 10 --hostfile mpi_hosts ./t4 720p.jpg 720psobel.jpg
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 12 --hostfile mpi_hosts ./t4 720p.jpg 720psobel.jpg
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 15 --hostfile mpi_hosts ./t4 720p.jpg 720psobel.jpg
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 16 --hostfile mpi_hosts ./t4 720p.jpg 720psobel.jpg
+
 mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 1 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
 mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 2 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
 mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 3 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
 mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 4 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
 mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 5 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
 mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 6 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
-mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 7 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
 mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 8 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
 mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 9 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
 mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 10 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
-mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 11 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
 mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 12 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
-mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 13 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
-mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 14 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
 mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 15 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
 mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 16 --hostfile mpi_hosts ./t4 1080p.jpg 1080psobel.jpg 
 
-
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 1 --hostfile mpi_hosts ./t4 4k.jpg 4ksobel.jpg 
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 2 --hostfile mpi_hosts ./t4 4k.jpg 4ksobel.jpg 
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 3 --hostfile mpi_hosts ./t4 4k.jpg 4ksobel.jpg 
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 4 --hostfile mpi_hosts ./t4 4k.jpg 4ksobel.jpg 
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 5 --hostfile mpi_hosts ./t4 4k.jpg 4ksobel.jpg 
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 6 --hostfile mpi_hosts ./t4 4k.jpg 4ksobel.jpg 
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 8 --hostfile mpi_hosts ./t4 4k.jpg 4ksobel.jpg 
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 9 --hostfile mpi_hosts ./t4 4k.jpg 4ksobel.jpg 
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 10 --hostfile mpi_hosts ./t4 4k.jpg 4ksobel.jpg 
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 12 --hostfile mpi_hosts ./t4 4k.jpg 4ksobel.jpg 
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 15 --hostfile mpi_hosts ./t4 4k.jpg 4ksobel.jpg 
+mpirun --allow-run-as-root  --mca btl_base_warn_component_unused 0 -np 16 --hostfile mpi_hosts ./t4 4k.jpg 4ksobel.jpg 
